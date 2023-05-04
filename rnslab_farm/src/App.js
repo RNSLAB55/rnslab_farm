5e8255f736bc9b3f04cf2f67745c7230672e67ce
username <hmc041206@gmail.com>
2023-05-04 15:23:26 +0900

    codeSplitting

--- rnslab_farm/src/App.js
+++ rnslab_farm/src/App.js
@@ -1,21 +1,26 @@
+import React, {lazy, Suspense} from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from "./page/login";
-import Type from "./page/type";
-import Main from "./page/main/main";
-import EachSensor from './page/main/components/everSensor/eachSensor/eachSensor';
+// import Type from "./page/type";
+// import Main from "./page/main/main";
+// import EachSensor from './page/main/components/everSensor/eachSensor/eachSensor';
 import ScrollToTop from './ScrollTop';
+
+const Type = lazy(async () => await import('./page/type'));
+const Main = lazy(async () => await import('./page/main/main'));
+const EachSensor = lazy(async () => await import('./page/main/components/everSensor/eachSensor/eachSensor'));
+
 
 function App() {
   return (
-    
       <Router>
         <ScrollToTop />
         <Routes>
           <Route path={"/"} element={<Login />}></Route>
-          <Route path={"/type"} element={<Type />}></Route>
-          <Route path={"/main"} element={<Main />}></Route>
-          <Route path={"/eachSensor/:item"} element={<EachSensor />}></Route>
+          <Route path={"/type"} element={<Suspense fallback={<></>}><Type/></Suspense>}></Route>
+          <Route path={"/main"} element={<Suspense fallback={<></>}><Main/></Suspense>}></Route>
+          <Route path={"/eachSensor/:item"} element={<Suspense fallback={<></>}><EachSensor/></Suspense>}></Route>
         </Routes>
       </Router>
   );
